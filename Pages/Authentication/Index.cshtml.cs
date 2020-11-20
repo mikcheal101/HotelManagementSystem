bc495d347582711f781611782f3812661bcf6e2b
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-20 15:10:45 +0100

    hemen/Authentication pages and model created

---
+++ Pages/Authentication/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelManagementSystem.Pages.Authentication
+{
+    public class IndexModel : PageModel
+    {
+
+        public class FormModel
+        {
+            [Required]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
+            [StringLength(10, ErrorMessage = "The {0} must have a length between {2} and {1}", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember Me")]
+            public bool RememberMe { get; set; }
+        }
+
+        [BindProperty]
+        public FormModel Input { get; set; }
+
+        public void OnGet()
+        { }
+    }
+}
