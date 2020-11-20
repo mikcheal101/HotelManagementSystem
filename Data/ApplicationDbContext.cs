cfb25b6b0bfc329a35618e950aed5259a7afbffc
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-20 15:42:25 +0100

    hemen/db updated with seeder

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,7 +1,9 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
+using System.Diagnostics;
 using HotelManagementSystem.Models;
 
 namespace HotelManagementSystem.Data
@@ -15,6 +17,23 @@ namespace HotelManagementSystem.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            PasswordHasher<Person> passwordHasher = new PasswordHasher<Person>();
+
+            // create default user
+            var user = new Person
+            {
+                UserName = "admin@mail.com",
+                Email = "admin@mail.com",
+                EmailConfirmed = true,
+                Firstname = "Hassan",
+                PhoneNumber = "07020464737",
+                PhoneNumberConfirmed = true
+            };
+
+            user.PasswordHash = passwordHasher.HashPassword(user, "password");
+
+            // seed user
+            builder.Entity<Person>().HasData(user);
         }
     }
 }
