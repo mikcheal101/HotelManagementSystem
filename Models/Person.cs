da38aaa569f517b4ec4d46edb878acdea1b6e0aa
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-21 03:21:41 +0100

    hemen/person model updated

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,10 +1,22 @@
+using System;
 using Microsoft.AspNetCore.Identity;
 
 namespace HotelManagementSystem.Models
 {
     public class Person : IdentityUser
     {
+        public enum PersonType
+        {
+            EmployeeAdmin = 0,
+            Employee = 1,
+            Customer = 2,
+        };
+
+
+        [PersonalData]
+        public PersonType UserType { get; set; }
         [PersonalData]
         public string Firstname { get; set; }
+
     }
 }
