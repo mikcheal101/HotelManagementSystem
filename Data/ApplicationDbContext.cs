bd788a5409270cb64e0e590ec5a9cee70ec4b88d
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-21 23:45:07 +0100

    Hemen/ - Database seeder for super admin and usertypes - Person Model created - Usertype Model Created

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using System;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
@@ -10,18 +11,56 @@ namespace HotelManagementSystem.Data
 {
     public class ApplicationDbContext : IdentityDbContext<Person>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        { }
 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        public DbSet<Usertype> Usertypes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             PasswordHasher<Person> passwordHasher = new PasswordHasher<Person>();
 
+            // create the usertypes
+            var ADMIN_ID = Guid.NewGuid().ToString();
+            var adminUsertype = new Usertype
+            {
+                Id = ADMIN_ID,
+                ConcurrencyStamp = ADMIN_ID,
+                Name = "system administrator".ToLower(),
+                NormalizedName = "system administrator".ToUpper(),
+            };
+
+            var EMPLOYEE_ID = Guid.NewGuid().ToString();
+            var employeeUsertype = new Usertype
+            {
+                Id = EMPLOYEE_ID,
+                ConcurrencyStamp = EMPLOYEE_ID,
+                Name = "employee".ToLower(),
+                NormalizedName = "employee".ToUpper(),
+            };
+
+            var CUSTOMER_ID = Guid.NewGuid().ToString();
+            var customerUsertype = new Usertype
+            {
+                Id = CUSTOMER_ID,
+                ConcurrencyStamp = CUSTOMER_ID,
+                Name = "customer".ToLower(),
+                NormalizedName = "customer".ToUpper(),
+            };
+
+            // seed usertpes
+            builder.Entity<Usertype>().HasData(adminUsertype, employeeUsertype, customerUsertype);
+
             // create default user
+            var USER_ID = Guid.NewGuid().ToString();
             var user = new Person
             {
+                Id = USER_ID,
                 UserName = "admin@mail.com".ToLower(),
                 Email = "admin@mail.com".ToLower(),
                 NormalizedUserName = "admin@mail.com".ToUpper(),
@@ -29,13 +68,20 @@ namespace HotelManagementSystem.Data
                 EmailConfirmed = true,
                 Firstname = "Hassan",
                 PhoneNumber = "07020464737",
-                PhoneNumberConfirmed = true
+                PhoneNumberConfirmed = true,
             };
-
             user.PasswordHash = passwordHasher.HashPassword(user, "P@ssw0rd");
 
             // seed user
             builder.Entity<Person>().HasData(user);
+
+            // add user to role
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+
+                RoleId = ADMIN_ID,
+                UserId = USER_ID
+            });
         }
     }
 }
--- Models/Person.cs
+++ Models/Person.cs
@@ -5,18 +5,11 @@ namespace HotelManagementSystem.Models
 {
     public class Person : IdentityUser
     {
-        public enum PersonType
-        {
-            EmployeeAdmin = 0,
-            Employee = 1,
-            Customer = 2,
-        };
-
-
-        [PersonalData]
-        public PersonType UserType { get; set; }
         [PersonalData]
         public string Firstname { get; set; }
+
+        [PersonalData]
+        public Usertype PersonType { get; set; }
 
     }
 }
