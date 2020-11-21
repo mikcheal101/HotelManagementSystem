975ac3b5c77e2ac477f4291238918457d02d8235
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-21 02:42:37 +0100

    hemen/Secured folder with boiler plate pages added

---
+++ Pages/Secured/Dashboard/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotelManagementSystem.Pages.Secured.Dashboard
+{
+    public class DashboardPageModel : PageModel
+    {
+
+        public void OnGet()
+        { }
+    }
+}
