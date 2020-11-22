44a1fb6977e79c7f8b08be4f4887fbee08b64ead
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-22 11:41:30 +0100

    hemen/Rooms Category - completed

--- Pages/Secured/Rooms/Category/Index.cshtml.cs
+++ Pages/Secured/Rooms/Category/Index.cshtml.cs
@@ -5,12 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using Microsoft.Extensions.Logging;
+using HotelManagementSystem.Data;
+using HotelManagementSystem.Models;
+using System.ComponentModel.DataAnnotations;
+
 namespace HotelManagementSystem.Pages.Secured.Rooms.Category
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext dbContext;
+        private readonly ILogger<IndexModel> logger;
+
+        public ICollection<RoomCategory> RoomCategories { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext dbContext)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
         public void OnGet()
         {
+            this.RoomCategories = this.dbContext.RoomCategories.ToList<RoomCategory>();
         }
     }
 }
