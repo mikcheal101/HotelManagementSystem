c2184275f5dc1cc9a28c5f2b994833ca10d3ccdd
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-22 11:51:07 +0100

    hemen/Rooms Routes updated

--- Pages/Secured/Rooms/Index.cshtml.cs
+++ Pages/Secured/Rooms/Index.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using HotelManagementSystem.Data;
+using HotelManagementSystem.Models;
+using System.ComponentModel.DataAnnotations;
+using  Microsoft.Extensions.Logging;
 namespace HotelManagementSystem.Pages.Secured.Rooms
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> logger;
+        private readonly ApplicationDbContext dbContext;
+
+        public ICollection<Room> Rooms { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger,  ApplicationDbContext dbContext)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
         public void OnGet()
         {
+            this.Rooms = this.dbContext.Rooms.ToList<Room>();
         }
     }
 }
