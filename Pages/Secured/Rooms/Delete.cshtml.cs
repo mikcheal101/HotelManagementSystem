e5fa26811294f7de1356e89fc41e579b68562ba5
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-22 18:57:32 +0100

    hemen/delete room completed

--- Pages/Secured/Rooms/Delete.cshtml.cs
+++ Pages/Secured/Rooms/Delete.cshtml.cs
@@ -5,12 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using Microsoft.Extensions.Logging;
+using HotelManagementSystem.Data;
+using HotelManagementSystem.Models;
+using System.ComponentModel.DataAnnotations;
+
 namespace HotelManagementSystem.Pages.Secured.Rooms
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<DeleteModel> logger;
+        private readonly ApplicationDbContext dbContext;
+
+        public string ReturnUrl { get; set; }
+
+        public DeleteModel(ILogger<DeleteModel> logger, ApplicationDbContext dbContext)
         {
+            this.logger = logger;
+            this.dbContext = dbContext;
+            this.ReturnUrl = "/Secured/Rooms/Index";
+        }
+
+        public async Task<IActionResult> OnGet(string Id)
+        {
+            Room room = await this.dbContext.Rooms.FindAsync(Id);
+            if (room != null)
+            {
+                this.dbContext.Remove<Room>(room);
+                await this.dbContext.SaveChangesAsync();
+
+            }
+            return Redirect(this.ReturnUrl);
         }
     }
 }
