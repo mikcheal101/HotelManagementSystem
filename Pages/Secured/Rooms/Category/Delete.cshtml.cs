6d89fcbbce2b74c34c9205d737fb2a4c8c2c0ed0
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-22 13:02:18 +0100

    hemen/rooms category delete completed

--- Pages/Secured/Rooms/Category/Delete.cshtml.cs
+++ Pages/Secured/Rooms/Category/Delete.cshtml.cs
@@ -5,12 +5,41 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
+using Microsoft.Extensions.Logging;
+using HotelManagementSystem.Data;
+using HotelManagementSystem.Models;
+using System.ComponentModel.DataAnnotations;
+
 namespace HotelManagementSystem.Pages.Secured.Rooms.Category
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<DeleteModel> logger;
+        private readonly ApplicationDbContext dbContext;
+
+        public string ReturnUrl { get; set; }
+
+        public DeleteModel(ILogger<DeleteModel> logger, ApplicationDbContext dbContext)
         {
+            this.logger = logger;
+            this.dbContext = dbContext;
+            this.ReturnUrl = "/Secured/Rooms/Category/Index";
+        }
+
+        public async Task<IActionResult> OnGetAsync(string Id)
+        {
+            var roomCategory = await this.dbContext.RoomCategories.FindAsync(Id);
+            if (roomCategory != null)
+            {
+                this.dbContext.Remove<RoomCategory>(roomCategory);
+                var deleted = await this.dbContext.SaveChangesAsync();
+                if (deleted == 1)
+                {
+                    this.logger.LogInformation("Room Category Deleted Successfully!");
+                }
+            }
+            return Redirect(this.ReturnUrl);
         }
     }
 }
