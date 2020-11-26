0e749ec55ac0f7bdd8aeb91abe38b908f16fd66d
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-26 11:23:09 +0100

    hemen/Rooms pages updated

--- Pages/Secured/Rooms/Index.cshtml.cs
+++ Pages/Secured/Rooms/Index.cshtml.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using HotelManagementSystem.Data;
 using HotelManagementSystem.Models;
-using  Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 namespace HotelManagementSystem.Pages.Secured.Rooms
 {
     public class IndexModel : PageModel
@@ -15,15 +16,19 @@ namespace HotelManagementSystem.Pages.Secured.Rooms
 
         public ICollection<Room> Rooms { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger,  ApplicationDbContext dbContext)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext dbContext)
         {
             this.logger = logger;
             this.dbContext = dbContext;
-            this.Rooms = this.dbContext.Rooms.ToList<Room>();
         }
 
         public async Task OnGetAsync()
         {
+            this.Rooms = await this.dbContext.Rooms
+                .Include(room => room.RoomCategory)
+                .AsNoTracking()
+                .ToListAsync<Room>();
+
             foreach (var room in this.Rooms)
             {
                 room.RoomCategory = await this.dbContext.RoomCategories.FindAsync(room.RoomCategoryId);
