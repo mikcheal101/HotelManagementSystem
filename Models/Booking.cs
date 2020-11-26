dd89a354ca6aa023c435db251c64ada8b97eec3e
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-26 23:46:55 +0100

    heen/.

---
+++ Models/Booking.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+using HotelManagementSystem.Models;
+
+namespace HotelManagementSystem.Models
+{
+    public class Booking
+    {
+        [Key]
+        public string Id { get; set; }
+        public Customer Customer { get; set; }
+        public Room Room { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime CheckOutTime { get; set; }
+        public DateTime ExpectedCheckOutTime { get; set; }
+        public Person Attendant { get; set; }
+        public bool PaidPenalty { get; set; }
+
+        public float Penalty
+        {
+            get
+            {
+                var overlap = this.CheckOutTime - this.ExpectedCheckOutTime;
+                return overlap.Days * this.Room.RoomCategory.CostPerNight;
+            }
+        }
+    }
+}
