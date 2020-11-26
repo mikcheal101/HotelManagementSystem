1ed35e729886a6d4b57d10b35f09a763f193c244
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-26 12:35:16 +0100

    hemen/updated customer model

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -10,16 +10,16 @@ namespace HotelManagementSystem.Models
     }
     public class Customer : Person
     {
-        [Required(ErrorMessage="A Type of Means Of Identification is required!")]
+        [Required(ErrorMessage = "A Type of Means Of Identification is required!")]
         [PersonalData]
         public IdentificationType MeansOfIdentificationType { get; set; }
 
-        [Required(ErrorMessage="Means of identification number is required!")]
+        [Required(ErrorMessage = "Means of identification number is required!")]
         [PersonalData]
         public string IdentificationNumber { get; set; }
 
 
-        [Required(ErrorMessage="Kindly Upload A Means Of Identification!")]
+        [Required(ErrorMessage = "Kindly Upload A Means Of Identification!")]
         [PersonalData]
         public string MeansOfIdentification { get; set; }
     }
