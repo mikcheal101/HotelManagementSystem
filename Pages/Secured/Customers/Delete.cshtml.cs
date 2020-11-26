552efe859360021727dfa25fa6f447709aef66ce
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-26 12:46:38 +0100

    hemen/Customers delete page route completed

--- Pages/Secured/Customers/Delete.cshtml.cs
+++ Pages/Secured/Customers/Delete.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using HotelManagementSystem.Data;
+using HotelManagementSystem.Models;
+using Microsoft.Extensions.Logging;
 namespace HotelManagementSystem.Pages.Secured.Customers
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<DeleteModel> logger;
+        private readonly ApplicationDbContext dbContext;
+        private readonly string ReturnUrl;
+        public DeleteModel(ILogger<DeleteModel> logger, ApplicationDbContext dbContext)
         {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.ReturnUrl = "/Secured/Customers/Index";
+        }
+        public async Task<IActionResult> OnGetAsync(string Id)
+        {
+            var customer = await this.dbContext.Customers.FindAsync(Id);
+            this.dbContext.Remove<Customer>(customer);
+            await this.dbContext.SaveChangesAsync();
+            return Redirect(this.ReturnUrl);
         }
     }
 }
