6c415c6daeda492efc070e40c09de6a14f9d54fa
Micheal Hirekaan <7416948-mikkytionze@users.noreply.gitlab.com>
2020-11-30 01:19:07 +0100

    hemen/bookings listing

---
+++ Pages/Secured/Bookings/Index.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using HotelManagementSystem.Data;
+using HotelManagementSystem.Models;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Logging;
+
+namespace HotelManagementSystem.Pages.Secured.Bookings
+{
+    public class IndexPage : PageModel
+    {
+        private readonly ILogger<IndexPage> logger;
+        private readonly ApplicationDbContext dbContext;
+        public IndexPage(ApplicationDbContext dbContext, ILogger<IndexPage> logger)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        [BindProperty]
+        public ICollection<Booking> Bookings { get; set; }
+
+        public void OnGet()
+        {
+            this.Bookings = this.dbContext.Bookings
+                .Include(Booking => Booking.Attendant)
+                .Include(Booking => Booking.Customer)
+                .Include(Booking => Booking.Room)
+                .ToList<Booking>();
+        }
+    }
+}
